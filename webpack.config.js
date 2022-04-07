48880ad14eeb40754c025cb4fab03e442371d534
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-04-06 23:50:10 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -48,6 +49,7 @@ module.exports = {
         ],
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
         filename: "main.bundle.css"
         }),
