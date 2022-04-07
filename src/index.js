dd932db95ec185e3577063179742f40f43468823
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-04-06 23:55:02 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-//import './components/Card.js';
+import './components/Card.js';
 
 render(<App />, document.getElementById('root'));
