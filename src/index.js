6f60ab90316bfc3beaaf5fb0831e28ce45c09dc0
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-04-07 21:43:59 -0600

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './App';
-import './components/Card.js';
-
-render(<App />, document.getElementById('root'));
